b4fdafd25472438df12a690151651082627c39ea
sakuya10969 <sakuya.f20010618@gmail.com>
2025-08-10 17:30:34 +0900

    feat: constants

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+import type { NavItem } from "@/types";
+
+export const NAV: NavItem[] = [
+  { label: "TOPS", href: "#tops" },
+  { label: "INTRODUCTION", href: "#introduction" },
+  { label: "NEWS", href: "#news" },
+  { label: "CAST", href: "#cast" },
+  { label: "GOODS", href: "#goods" },
+];
