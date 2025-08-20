0712017099f0e07ec0a5739b2bbb43a3572692b3
sakuya10969 <sakuya.f20010618@gmail.com>
2025-08-20 12:47:27 +0900

    feat: page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import TopCatch from './components/top/TopCatch'
+import Header from '@/components/Header'
+import Main from '@/components/Main'
+
+const LogoComponent = () => (
+  <img
+    src="/assets/logo-reiwaroman.svg"
+    alt="REIWAROMAN"
+    className="w-9 h-9"
+  />
+)
 
 function App() {
   return (
-    <TopCatch />
+    <div className="w-full">
+      <Header Logo={LogoComponent} />
+      <Main />
+    </div>
   )
 }
 
