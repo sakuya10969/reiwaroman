ffe4787fa1400a8cc3f9659c79ca7623997523cb
sakuya10969 <sakuya.f20010618@gmail.com>
2025-08-27 10:47:43 +0900

    fix: tops

--- src/components/top/TopsCatch.tsx
+++ src/components/top/TopsCatch.tsx
@@ -3,14 +3,14 @@ import logo_5 from "@/assets/logo_5.png";
 
 const TopsCatch = () => {
   return (
-    <div className="flex relative w-full h-full bg-black text-white text-center pt-30 flex-col justify-center">
+    <div className="flex relative w-full h-full bg-black text-white text-center pt-24 flex-col justify-center">
       {/* テキストブロック */}
       <div className="pb-4 sm:px-6 w-[90vw] mx-auto flex flex-col items-center">
         <div>
           {TOPS_CATCH_TITLE_LINES.map((line, index) => (
             <p
               key={index}
-              className="mx-auto text-2xl md:text-5xl lg:text-6xl
+              className="mx-auto text-3xl md:text-5xl lg:text-6xl
                          font-extrabold leading-[1.1] tracking-tight whitespace-nowrap scale-x-70"
               style={{ fontFamily: '"dnp-shuei-shogomincho-std", serif' }}
             >
@@ -23,7 +23,7 @@ const TopsCatch = () => {
           {TOPS_CATCH_SUBTITLE.map((line, index) => (
           <p
             key={index}
-            className="text-xs md:text-sm font-extrabold
+            className="text-sm md:text-base font-extrabold
                        uppercase whitespace-nowrap scale-y-80"
             style={{ fontFamily: 'Prompt, sans-serif' }}
           >
