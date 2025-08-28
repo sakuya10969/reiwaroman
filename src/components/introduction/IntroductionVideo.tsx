c4b3081cec8982fadffdc9d0e91c329ba0a3083b
sakuya10969 <sakuya.f20010618@gmail.com>
2025-08-28 13:32:52 +0900

    style: introduction video

--- src/components/introduction/IntroductionVideo.tsx
+++ src/components/introduction/IntroductionVideo.tsx
@@ -118,7 +118,7 @@ const IntroductionVideo = ({ videoId = "S6hol1r6xSc" }: IntroductionVideoProps) 
   return (
     <div
       ref={containerRef}
-      className="relative w-full isolate min-h-[50vh] lg:h-screen landscape:h-screen pt-16 md:pt-24 overflow-hidden bg-black"
+      className="relative w-full isolate min-h-[50vh] lg:h-screen landscape:h-screen overflow-hidden bg-black"
       aria-hidden
       onMouseMove={showControls}
       onMouseLeave={hideControls}
